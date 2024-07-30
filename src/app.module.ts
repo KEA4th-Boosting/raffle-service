1455328e47b223593f853c83a42c8d0c1b901ae0
hanseul37 <hanseul37@daum.net>
2024-07-30 17:31:16 +0900

    BS-5 [feat] 추첨 생성 API 구현 (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EntryModule } from './entry/entry.module';
+import { RaffleModule } from './raffle/raffle.module';
+import { Entry } from './entry/entities/entry.entity';
+import { Raffle } from './raffle/entities/raffle.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DATABASE,
+      entities: [Entry, Raffle],
+      synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+    }),
+    EntryModule,
+    RaffleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
