e21662e25478124e87e1679b93e93c761a1691b8
hanseul37 <hanseul37@daum.net>
2024-08-01 23:48:43 +0900

    BS-5 [refactor] entry, winner 엔티티 수정

--- src/entry/entry.controller.ts
+++ src/entry/entry.controller.ts
@@ -2,11 +2,14 @@ import {Body, Controller, Post, Put} from '@nestjs/common';
 import {EntryService} from "./entry.service";
 import {Entry} from "./entities/entry.entity";
 import {CreateEntryDto} from "./dto/create-entry.dto";
+import {ApiOperation, ApiTags} from "@nestjs/swagger";
 
+@ApiTags('entry')
 @Controller('entry')
 export class EntryController {
     constructor(private entryService: EntryService) {}
 
+    @ApiOperation({ summary: '응모 생성', description: '새로운 응모 항목을 생성합니다.'})
     @Post()
     async create(@Body() createEntryDto: CreateEntryDto): Promise<Entry> {
         return await this.entryService.create(createEntryDto);
