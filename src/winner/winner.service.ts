71494e9863a8d7c5a3e55c1a5370a6a1fbf43846
hanseul37 <hanseul37@daum.net>
2024-08-05 14:30:53 +0900

    BS-5 [feat] 당첨 생성, 예약 취소 API 구현

--- src/winner/winner.service.ts
+++ src/winner/winner.service.ts
@@ -2,6 +2,8 @@ import {Injectable, NotFoundException} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
 import {Winner} from "./entities/winner.entity";
 import {Repository} from "typeorm";
+import {CreateWinnerDto} from "./dto/create-winner.dto";
+import {UpdateWinnerDto} from "./dto/update-winner.dto";
 
 @Injectable()
 export class WinnerService {
@@ -9,6 +11,11 @@ export class WinnerService {
         @InjectRepository(Winner)
         private winnerRepository: Repository<Winner>,
     ) {}
+
+    async create(createWinnerDto: CreateWinnerDto): Promise<Winner> {
+        const newWinner = this.winnerRepository.create(createWinnerDto);
+        return await this.winnerRepository.save(newWinner);
+    }
 
     async findOne(winnerId: number): Promise<Winner> {
         const winner = await this.winnerRepository.findOne({
@@ -20,5 +27,10 @@ export class WinnerService {
         }
 
         return winner;
+    }
+
+    async update(winnerId: number, updateWinnerDto: UpdateWinnerDto): Promise<Winner> {
+        await this.winnerRepository.update(winnerId, updateWinnerDto);
+        return this.findOne(winnerId);
     }
 }
