fe1dc844d6a4d749d2cbbb969842e4ee317d3fab
hanseul37 <hanseul37@daum.net>
2024-08-14 00:23:12 +0900

    BS-5 [feat] 유저 당첨 확률 조회 API 구현

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import {ConfigService} from "@nestjs/config";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  //app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const config = new DocumentBuilder()
       .setTitle('Raffle-swagger')
